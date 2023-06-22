5e5df34ecd8b4b70ee5a99e827543bbdd2afa3c0
LBJANU <59941227+LBJANU@users.noreply.github.com>
2023-06-22 11:10:34 -0400

    Navigation so that Signup and Login Page is poss?

--- App.js
+++ App.js
@@ -1,70 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import HomeScreen from "./HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [name, setName] = useState('Shaun');
-  const {change, setChange} = useState('default');
-  const {changeage, setChangeAge} = useState('0');
-  const [age, setAge] = useState('30');
-
-  const clickPressed = () => {
-    setName('change');
-    setAge('changeage');
-  }
-
-
   return (
-    <View style={styles.container}>
-      <View style ={styles.header}>
-      
-        <Text style = {styles.bolden}> Enter Name:</Text>
-        <TextInput style = {styles.input}
-         placeholder='Example: Anish Jain' 
-         onChangeText={(change) => setChange(change)} />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name = "Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        //  options={{title: "Welcome"}}
 
-        <Text style = {styles.bolden}> Enter Age:</Text>
-        <TextInput style = {styles.input}
-         placeholder='Example: 23' 
-         onChangeText={(changeage) => setChangeAge(changeage)} />
+        />
+       
+
+      </Stack.Navigator>
 
 
-      <Text style ={styles.bolden}>My name is {name} and my age is {age} </Text>
-      </View>
-      <View style = {styles.buttonContainer}>
-        <Text></Text>
-        <Button title = 'update state' onPress={clickPressed} />
-      </View>
-
-    </View>
- 
+    </NavigationContainer>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-
-  },
-  bolden: {
-    fontSize: 20,
-  },
-  buttonContainer: {
-    marginTop: 40,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  }
-});
---
+++ HomeScreen.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, Image} from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <ImageBackground
+    source = {require('./assets/background2.png')}
+    style = {styles.background}
+    resizeMode= "cover">
+
+    <View>
+      <Image
+        source = {require('./assets/logo3.jpeg')}
+        style = {styles.logo}>
+
+       </Image>
+       <Text style = {styles.text}>Learn More. Teach More. Help More.</Text>
+       <TouchableOpacity style = {{borderRadius: 20}}>
+        <Text style = {styles.signup}>Sign Up</Text>
+       </TouchableOpacity>
+       <TouchableOpacity style = {{borderRadius: 20}}>
+        <Text style = {styles.login}>Log In</Text>
+       </TouchableOpacity>
+    </View>
+  </ImageBackground>
+
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  background: {
+    flex: 1
+    
+  },
+  logo:{
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: "100%",
+    height: 240,
+    marginTop: '8%'
+  },
+  text:{
+    marginTop: '-0%',
+    marginLeft: '14.5%',
+    alignContent: 'center',
+    justifyContent: 'center',
+    color: '#92cdcd',
+    fontSize: 18
+
+  },
+  signup: {
+    backgroundColor: 'white',
+    color: '#3A59FF',
+    width: "75%",
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginLeft: '11%',
+    padding: "2%",
+    fontSize:  27,
+    marginTop: '105%'
+  },
+  login: {
+    backgroundColor: '#3A59FF',
+    color: 'white',
+    width: "75%",
+    borderRadius: 25,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginLeft: '11%',
+    padding: "2%",
+    fontSize:  27,
+    marginTop: '8%'
+  },
+ 
+});
